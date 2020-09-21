fcfe59fa7d4b78a607fbe2edda93eb255194dfb1
per <per7614@163.com>
2020-09-21 10:47:29 +0800

    9.21 忘记export了

--- src/components/Test/index.js
+++ src/components/Test/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // import PropTypes from 'prop-types';
 import { Text } from "react-native";
 
-class Index extends Component {
+export default class Index extends Component {
   render() {
     return <Text style={{ color: "red", fontSize: 30 }}>测试测试</Text>;
   }
